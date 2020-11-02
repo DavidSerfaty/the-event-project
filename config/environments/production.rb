8ae5f246025405e637fe87ed35b5834acc70c2a9
DavidSerfaty <david@owanted.io>
2020-11-02 19:43:43 +0100

    final 2 nov

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,11 +113,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
+      port: 587,
+      domain: 'gmail.com',
       user_name: ENV["GMAIL_LOGIN"],
       password: ENV["GMAIL_PWD"],
-      authentication:       'plain',
+      authentication: 'plain',
       enable_starttls_auto: true
     }
 
