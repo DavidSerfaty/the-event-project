aeb4a57a3843e19c5ba993097684c8a6a9bfd21b
DavidSerfaty <david@owanted.io>
2020-11-02 18:56:29 +0100

    mailer event

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,11 @@
 class Attendance < ApplicationRecord
   belongs_to :event
 	belongs_to :guest, class_name: "User"
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_event(self).deliver_now
+  end
+
 end
