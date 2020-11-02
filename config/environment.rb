40136a248ff0146fd64769b559edcf5ddd681b0b
DavidSerfaty <david@owanted.io>
2020-11-02 17:51:51 +0100

    test mailer 2

--- config/environment.rb
+++ config/environment.rb
@@ -13,16 +13,3 @@ Rails.application.initialize!
 #     :password           => ENV['GMAIL_PWD'],
 #     enable_starttls_auto: true
 #   }
-
-
-
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name: ENV["GMAIL_LOGIN"],
-  password: ENV["GMAIL_PWD"],
-  authentication:       'plain',
-  enable_starttls_auto: true
-}
