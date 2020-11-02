ce165e34c6fb260390d8cc48fc22c22c4d234701
DavidSerfaty <david@owanted.io>
2020-11-02 17:22:49 +0100

    gmail mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-
   has_many :events, foreign_key: 'owner_id', dependent: :destroy
   has_many :attendances, foreign_key: 'guest_id'
   has_many :attended_events, through: :attendances, source: :event
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
