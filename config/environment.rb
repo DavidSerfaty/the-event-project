5f45d3d979b10e1a267c18eaa969d6eba36642aa
DavidSerfaty <david@owanted.io>
2020-11-02 17:42:04 +0100

    starttls

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,29 @@ ActionMailer::Base.smtp_settings =   {
     :domain             => 'gmail.com', #you can also use google.com
     :authentication     => :plain,
     :user_name          => ENV['GMAIL_LOGIN'],
-    :password           => ENV['GMAIL_PWD']
+    :password           => ENV['GMAIL_PWD'],
+    enable_starttls_auto: true
   }
+
+
+  #
+  # config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address: ENV["host"],
+  #     port:                 587,
+  #     domain:               'gmail.com',
+  #     user_name: ENV["username"],
+  #     password: ENV["password"],
+  #     authentication:       'plain',
+  #     enable_starttls_auto: true
+  #   }
+  #
+  # config.action_mailer.smtp_settings = {
+  #     address: ENV["host"],
+  #     port:                 587,
+  #     domain:               'gmail.com',
+  #     user_name: ENV["username"],
+  #     password: ENV["password"],
+  #     authentication:       'plain',
+  #     enable_starttls_auto: true
+  #   }
