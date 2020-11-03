7449b2ba29493e403b0c5d4c0660b0b935a44c20
DavidSerfaty <david@owanted.io>
2020-11-03 22:24:01 +0100

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,19 +110,18 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com',
-      user_name: ENV["GMAIL_LOGIN"],
-      password: ENV["GMAIL_PWD"],
-      authentication: 'plain',
-      enable_starttls_auto: true
-    }
+  # config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #     address: 'smtp.gmail.com',
+  #     port: 587,
+  #     domain: 'gmail.com',
+  #     user_name: ENV["GMAIL_LOGIN"],
+  #     password: ENV["GMAIL_PWD"],
+  #     authentication: 'plain',
+  #     enable_starttls_auto: true
+  #   }
 
     config.action_mailer.perform_deliveries = true
-
     config.action_mailer.default_url_options = { host: 'https://the-event-project.herokuapp.com/' } # bien mettre le bon nom de domaine
 
 end
