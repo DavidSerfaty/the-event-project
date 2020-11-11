136cbe4ebf357eb37730dcc308cc3f5d9704f67c
DavidSerfaty <david@owanted.io>
2020-11-11 12:05:10 +0100

    using partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def bootstrap_class_for_flash(type)
+    case type
+      when 'notice' then "alert-info"
+      when 'success' then "alert-success"
+      when 'error' then "alert-danger"
+      when 'alert' then "alert-warning"
+    end
+  end
+
+  def bootstrap_class_for_status(status)
+    case status
+      when 'accepted' then "badge-success"
+      when 'pending' then "badge-warning"
+      when 'refused' then "badge-danger"
+    end
+  end
+  
 end
